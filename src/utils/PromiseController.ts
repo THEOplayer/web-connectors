1cc896b9211632188049823ed2af332b290c8f4b
Joris Fluyt <joris.fluyt@theoplayer.com>
2022-06-07 13:14:19 +0200

    Add PromiseController

---
+++ src/utils/PromiseController.ts
@@ -0,0 +1,39 @@
+export class PromiseController<T> {
+    readonly promise: Promise<T>;
+
+    private resolvePromise: ((result: T | PromiseLike<T>) => void) | undefined;
+
+    private rejectPromise: ((reason: any) => void) | undefined;
+
+    constructor() {
+        this.promise = new Promise<T>((resolve, reject) => {
+            this.resolve = resolve;
+            this.reject = reject;
+        });
+    }
+
+    resolve(result: T | PromiseLike<T>): void {
+        if (this.resolvePromise) {
+            this.resolvePromise(result);
+            this.destroy();
+        }
+    }
+
+    reject(reason: any): void {
+        if (this.rejectPromise) {
+            this.rejectPromise(reason);
+            this.destroy();
+        }
+    }
+
+    abort(): void {
+        this.promise.catch(() => {
+        });
+        this.reject(new Error());
+    }
+
+    private destroy() {
+        this.resolvePromise = undefined;
+        this.rejectPromise = undefined;
+    }
+}
