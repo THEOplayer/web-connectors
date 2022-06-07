36a90b6b9cb600faff796320ae1db7889e68e80e
Joris Fluyt <joris.fluyt@theoplayer.com>
2022-06-07 11:57:43 +0200

    Add SessionProperties

---
+++ src/yospace/SessionProperties.ts
@@ -0,0 +1,21 @@
+export interface SessionProperties {
+    new(): SessionProperties;
+
+    getAllowCorsForAnalytics(): boolean;
+    getApplyEncryptedTracking(): boolean;
+    getCustomHttpHeaders(): Map<string, string>;
+    getExcludedCategories(): number;
+    getFireHistoricalBeacons(): boolean;
+    getPrefetchResources(): boolean;
+    getRequestTimeout(): number;
+    getUserAgent(): string;
+
+    excludeFromSuppression(categories: number): void;
+    setAllowCorsForAnalytics(allowCorsForAnalytics: boolean): void;
+    setApplyEncryptedTracking(useHttps: boolean): void;
+    setCustomHttpHeaders(customHttpHeaders: Map<string, string>): void;
+    setFireHistoricalBeacons(fire: boolean): void;
+    setPrefetchResources(prefetch: boolean): void;
+    setRequestTimeout(requestTimeout: number): void;
+    setUserAgent(userAgent: string): void;
+}
