fe2d4d94752bf689d7407a78df8c393f1d46b454
Joris Fluyt <joris.fluyt@theoplayer.com>
2022-06-07 14:43:55 +0200

    Add abstract YospaceMetadataHandler

---
+++ src/integration/YospaceMetadataHandler.ts
@@ -0,0 +1,39 @@
+import {AddTrackEvent, TextTrack, TextTrackCue, TextTrackCueChangeEvent, TextTracksList} from 'theoplayer';
+
+export interface YospaceMetadata {
+    YMID: string;
+    YSEQ: string;
+    YTYP: string;
+    YDUR: string;
+    YCSP: string;
+}
+
+export type YospaceReport = Partial<YospaceMetadata>;
+
+export abstract class YospaceMetadataHandler {
+
+   private textTrackList: TextTracksList;
+
+    protected constructor(textTrackList: TextTracksList) {
+        this.textTrackList = textTrackList;
+        this.textTrackList.addEventListener('addtrack', this.handleAddTrack);
+    }
+
+    protected handleAddTrack = (event: AddTrackEvent) => {
+        const track = event.track as TextTrack;
+        if (track.kind !== 'metadata' || !track.cues) {
+            return;
+        }
+
+        track.addEventListener('cuechange', this.handleCueChange);
+    }
+
+    protected abstract isCorrectCueType: (cue: TextTrackCue) => boolean;
+
+    protected abstract handleCueChange: (cueChangeEvent: TextTrackCueChangeEvent) => void;
+
+    reset(): void {
+        this.textTrackList.forEach((track) => track.removeEventListener('cuechange', this.handleCueChange));
+        this.textTrackList.removeEventListener('addtrack', this.handleAddTrack);
+    }
+}
