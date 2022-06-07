8a175d509d90fffcb54b83591ef7066651bdf53b
Joris Fluyt <joris.fluyt@theoplayer.com>
2022-06-07 12:01:36 +0200

    Add AnalyticEventObserver

---
+++ src/yospace/AnalyticEventObserver.ts
@@ -0,0 +1,12 @@
+import {AdBreak, AdVert} from "./AdBreak";
+
+export interface AnalyticEventObserver {
+    onAnalyticUpdate: () => void;
+    onAdvertBreakEarlyReturn: (adBreak: AdBreak) => void;
+    onAdvertBreakStart: (adBreak: AdBreak) => void;
+    onAdvertBreakEnd: () => void;
+    onAdvertStart: (advert: AdVert) => void;
+    onAdvertEnd: () => void;
+    onSessionTimeout: () => void;
+    onTrackingEvent: (type: string) => void;
+}
