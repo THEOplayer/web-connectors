5ea5ee95bff5f5daefc8d668f7ef3717e6728ece
Jeroen Veltmans <jeroen.veltmans@theoplayer.com>
2022-07-01 11:47:01 +0200

    Add initial connector project code

---
+++ PACKAGE_README.md
@@ -0,0 +1,7 @@
+# conviva-connector-web
+
+A connector implementing Conviva for web.
+
+## Usage
+
+TO DO
---
+++ test/unit/example.spec.ts
@@ -0,0 +1,6 @@
+describe("My connector code", () => {
+    it("should be decently tested", () => {
+        const a: number = 3;
+        expect(a).toBe(3);
+    });
+});
