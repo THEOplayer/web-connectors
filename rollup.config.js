c998e7ccd8a716160cd9f745ec746b1867ec4f96
Mattias Buelens <mattias.buelens@theoplayer.com>
2022-07-06 14:22:56 +0200

    Deduplicate

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,21 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+const fileName = "yospace-connector";
+const globalName = "THEOplayerYospaceConnector";
+
+/**
+ * @type {import("rollup").RollupOptions[]}
+ */
 export default [{
     input: {
-        "yospace-connector": "src/index.ts"
+        [fileName]: "src/index.ts"
     },
     output: [
         {
             dir: "dist",
             entryFileNames: "[name].umd.js",
-            name: "THEOplayerYospaceConnector",
+            name: globalName,
             format: "umd",
             globals: { THEOplayer: "THEOplayer" }
         },
@@ -36,7 +42,7 @@ export default [{
     ]
 }, {
     input: {
-        "yospace-connector": "src/index.ts"
+        [fileName]: "src/index.ts"
     },
     output: [
         {
