1710aa7708c0fb69e34a27630c42650ba65d995c
Mattias Buelens <mattias.buelens@theoplayer.com>
2022-07-06 14:04:19 +0200

    Remove dist/bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,6 @@
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
-
-const { version } = require("./package.json");
 
 export default {
     input: {
@@ -21,24 +18,6 @@ export default {
             dir: "dist",
             entryFileNames: "[name].esm.js",
             format: "esm"
-        },
-        {
-            dir: "dist/bundle",
-            entryFileNames: `[name]-${version}.js`,
-            name: "connector_yospace_web",
-            format: "iife",
-            exports: "auto",
-            globals: { THEOplayer: "THEOplayer" },
-            plugins: [terser()]
-        },
-        {
-            dir: "dist/bundle",
-            entryFileNames: "[name]-latest.js",
-            name: "connector_yospace_web",
-            format: "iife",
-            exports: "auto",
-            globals: { THEOplayer: "THEOplayer" },
-            plugins: [terser()]
         }
     ],
     plugins: [
