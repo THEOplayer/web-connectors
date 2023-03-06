5888c9c20584560bdb126f78dfeebb535ef28e15
Jeroen Veltmans <jeroen.veltmans@theoplayer.com>
2023-03-06 13:39:08 +0100

    FIXUP add utils

---
+++ src/utils/Util.ts
@@ -0,0 +1,15 @@
+import { AdBreak } from "theoplayer";
+import { AdType } from "../nielsen/Types";
+
+export function getAdType(adBreak: AdBreak): AdType {
+    const currentAdBreakTimeOffset = adBreak.timeOffset;
+    let currentAdBreakPosition: AdType = 'ad';
+    if (currentAdBreakTimeOffset === 0) {
+        currentAdBreakPosition = 'preroll';
+    } else if (currentAdBreakTimeOffset < 0) {
+        currentAdBreakPosition = 'postroll';
+    } else if (currentAdBreakTimeOffset > 0) {
+        currentAdBreakPosition = 'midroll';
+    }
+    return currentAdBreakPosition;
+}
