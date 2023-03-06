a66088de92738a9815f8b876f601af10493fc819
Jeroen Veltmans <jeroen.veltmans@theoplayer.com>
2023-03-06 11:34:24 +0100

    Update types

--- src/nielsen/Types.ts
+++ src/nielsen/Types.ts
@@ -1,15 +1,20 @@
+export type AdType = 'preroll' | 'midroll' | 'postroll' | 'ad';
+
 export type NielsenOptions = {
     containerId?: string,
     nol_sdkDebug?: string,
     optout?: boolean
 }
 
+/**
+ * adModel: 1) - Linear – matches TV ad load * 2) Dynamic – Dynamic Ad Insertion (DAI)
+ */
 export type ContentMetadata = {
     type: 'content',
     adModel: '1' | '2'
-} & { [key: string]: string }
+} & { [ key: string ]: string }
 
 export type AdMetadata = {
-    type: 'preroll' | 'midroll' | 'postroll' | 'ad',
+    type: AdType,
     assetid: any // TODO string? or can be anything?
-} & { [key: string]: string }
+} & { [ key: string ]: string }
