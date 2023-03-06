9df153e550418a01fc3c3c21f424613ce611e48e
Jeroen Veltmans <jeroen.veltmans@theoplayer.com>
2023-03-06 11:47:53 +0100

    Add remaining functionality

--- src/integration/NielsenConnector.ts
+++ src/integration/NielsenConnector.ts
@@ -1,6 +1,15 @@
-import { AddTrackEvent, ChromelessPlayer, TextTrack, TextTrackCueChangeEvent, VolumeChangeEvent } from "theoplayer";
+import {
+    Ad,
+    AddTrackEvent,
+    ChromelessPlayer,
+    Event,
+    TextTrack,
+    TextTrackEnterCueEvent,
+    VolumeChangeEvent
+} from "theoplayer";
 import { loadNielsenLibrary } from "../nielsen/NOLBUNDLE";
-import { AdMetadata, ContentMetadata, NielsenOptions } from "../nielsen/Types";
+import { ContentMetadata, NielsenOptions } from "../nielsen/Types";
+import { getAdType } from "../utils/Util";
 
 export class NielsenConnector {
 
@@ -15,7 +24,7 @@ export class NielsenConnector {
         this.addEventListeners();
     }
 
-    updateMetadata(metadata: { [key: string]: string }): void {
+    updateMetadata(metadata: { [ key: string ]: string }): void {
         this.nSdkInstance.ggPM('updateMetadata', metadata);
     }
 
@@ -24,8 +33,13 @@ export class NielsenConnector {
         this.player.addEventListener('loadedmetadata', this.onLoadMetadata);
         this.player.addEventListener('ended', this.onEnd);
         this.player.addEventListener('sourcechange', this.onEnd);
+        this.player.addEventListener('play', this.onPlay);
 
         this.player.textTracks.addEventListener('addtrack', this.onAddTrack);
+
+        if (this.player.ads) {
+            this.player.ads.addEventListener('adbegin', this.onAdBegin);
+        }
     }
 
     private removeEventListeners(): void {
@@ -47,8 +61,7 @@ export class NielsenConnector {
         console.log('SENDING LOADMETADATA');
         const data: ContentMetadata = {
             type: 'content',
-            adModel: '1',
-            custom: 'value'
+            adModel: '1'
         }
         this.nSdkInstance.ggPM('loadmetadata', data);
     }
@@ -56,28 +69,61 @@ export class NielsenConnector {
     private onAddTrack = (event: AddTrackEvent) => {
         console.log('added track', event.track);
         if (event.track.kind === 'metadata') {
-            const track = (event.track as TextTrack);
+            const track = ( event.track as TextTrack );
             if (track.type === 'id3' || track.type === 'emsg') {
                 if (track.mode === 'disabled') {
                     track.mode = 'hidden';
                 }
-                track.addEventListener('cuechange', this.onCueChange);
+                track.addEventListener('entercue', this.onEnterCue);
             }
-
         }
     }
 
-    private onCueChange = (event: TextTrackCueChangeEvent) => {
-        console.log('oncuechange', event);
+    // On add cue? exit cue? enter cue??? Enter cue seems best tbh
+    private onEnterCue = (event: TextTrackEnterCueEvent) => {
+        const { cue } = event;
+        if (cue.track.type === 'id3') {
+            if (cue.content.id === 'PRIV' && cue.content.ownerIdentifier.indexOf('www.nielsen.com') !== -1) {
+                this.nSdkInstance.ggPM('sendID3', cue.content.ownerIdentifier);
+            }
+        } else {
+            // DASH emsg
+            // TODO test once we get DASH stream, verify if below is valid (with changes as I got it from old integration)
+            // if (cue.schemeIDURI && cue.schemeIDURI.indexOf(NIELSEN_ID) > -1) {
+            //     this.nielsenApi.sendId3Payload(cue.content);
+            // }
+        }
     }
 
     private onEnd = () => {
-        const playHeadPosition = Math.floor(this.player.currentTime).toString()
-        this.nSdkInstance.ggPM('end', playHeadPosition);
+        this.nSdkInstance.ggPM('end', this.getPlayHeadPosition());
+    }
+
+    private onPlay = () => {
+        // TODO pass metadataObject instead? https://engineeringportal.nielsen.com/docs/play_(Browser)
+        this.nSdkInstance.ggPM('play', this.getPlayHeadPosition());
+    }
+
+    private onAdBegin = (event: Event) => {
+        const currentAd = this.player.ads!.currentAds.filter((ad: Ad) => ad.type === 'linear');
+        if (currentAd.length !== 1) {
+            console.error('WAIT I THOUGHT WE WOULD HAVE A LINEAR AD, lets hope it is only nonlinear!', currentAd, this.player.ads!.currentAds);
+        }
+
+        const type = getAdType(this.player.ads!.currentAdBreak!);
+        const adMetadata = {
+            type,
+            // TODO check if id is always filled
+            assetid: currentAd[ 0 ].id
+        }
+        this.nSdkInstance.ggPM('loadMetadata', adMetadata);
+    }
+
+    private getPlayHeadPosition(): string {
+        return Math.floor(this.player.currentTime).toString();
     }
 
     destroy() {
-        this.nSdkInstance.destroy() // TODO check if this is how it should be? also what about player?
         this.removeEventListeners();
     }
 }
