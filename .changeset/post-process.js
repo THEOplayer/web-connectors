88fb5daf86020da10a483099fdb979db7b376acb
Mattias Buelens <649348+MattiasBuelens@users.noreply.github.com>
2024-02-16 14:38:15 +0100

    Use Changesets for changelog and releases (#1)

---
+++ .changeset/post-process.js
@@ -0,0 +1,13 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+for (const workspace of require('../package.json').workspaces) {
+    const changelogPath = path.join(__dirname, '..', workspace, 'CHANGELOG.md');
+    if (!fs.existsSync(changelogPath)) continue;
+    let changelog = fs.readFileSync(changelogPath, 'utf8');
+    changelog = changelog
+        .replace(/^### Major Changes/gm, '### 💥 Breaking Changes')
+        .replace(/^### Minor Changes/gm, '### ✨ Features')
+        .replace(/^### Patch Changes/gm, '### 🐛 Issues');
+    fs.writeFileSync(changelogPath, changelog);
+}
