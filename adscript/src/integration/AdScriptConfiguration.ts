eb34bb2dcb4f8a9036a6ceccd6c401242df49dfc
George Choustoulakis <george.choustoulakis@theoplayer.com>
2024-07-19 15:12:44 +0200

    Add docs on configuration

--- adscript/src/integration/AdScriptConfiguration.ts
+++ adscript/src/integration/AdScriptConfiguration.ts
@@ -1,9 +1,28 @@
 import type { Ad } from 'theoplayer';
 import { EmbeddedContentMetadata } from '../adscript/AdScript';
 
+/**
+ * The configuration for the AdScript Connector.
+ */
 export interface AdScriptConfiguration {
+    /**
+     * Integration ID you received from Nielsen representative
+     */
     implementationId: string;
-    i12n: { [key: string]: string };
-    debug?: boolean;
+
+    /**
+     * Additional information settings (additional information)
+     * For more information, see the [Additional Information Settings](https://adscript.admosphere.cz/en_adScript_browser.html) section.
+     */
+    i12n?: { [key: string]: string };
+
+    /**
+     * An optional advertisement processor to receive metadata about the Ad.
+     */
     adProcessor?: (ad: Ad) => EmbeddedContentMetadata;
+
+    /**
+     * Whether the connector should log all actions.
+     */
+    debug?: boolean;
 }
