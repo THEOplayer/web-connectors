985502471134253b12cc330d88ad209dfcf61d39
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-10-09 17:11:41 +0200

    Switch to Vitest

--- cmcd/test/unit/CMCDPayloadUtils.spec.ts
+++ cmcd/test/unit/CMCDPayloadUtils.spec.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from 'vitest';
 import { CMCDObjectType, CMCDStreamingFormat, CMCDStreamType } from '@theoplayer/cmcd-connector-web';
 import { transformToQueryParameters } from '../../src/CMCDPayloadUtils';
 
