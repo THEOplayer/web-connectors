5f354202d74c51259d9a56dcc68f7f7c52f75fef
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-10-24 14:41:35 +0200

    Make Conviva import handling more robust

--- conviva/src/utils/ConvivaSdk.ts
+++ conviva/src/utils/ConvivaSdk.ts
@@ -1,7 +1,9 @@
-// Use default import, so Rollup can insert its interop helpers.
-import Conviva from '@convivainc/conviva-js-coresdk';
+import * as Conviva from '@convivainc/conviva-js-coresdk';
 
-// Re-create Conviva's namespace.
-const { Analytics, Constants } = Conviva;
+// Conviva is distributed as CommonJS, so depending on the user's bundler choice
+// we will either get a single default export or separate named exports.
+const { Analytics, Constants } = 'Constants' in Conviva ? Conviva : ((Conviva as any).default as typeof Conviva);
+
+// Re-export the Conviva SDK as *actual* named exports.
 export { Analytics, Constants };
 export type * from '@convivainc/conviva-js-coresdk';
